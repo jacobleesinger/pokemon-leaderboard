39feac4de8417625f075020c651b8b1f87bcc025
Jacob Singer <jacobleesinger@gmail.com>
2024-07-29 22:43:41 -0700

    feat: add supabase auth

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -1,10 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
 import dotenv from '@dotenvx/dotenvx';
-
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL ?? '';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY ?? '';
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+
+// import { PRIVATE_SUPABASE_URL, PRIVATE_SUPABASE_ANON_KEY } from '$env/static/private';
+
+const PUBLIC_SUPABASE_URL = process.env.PUBLIC_SUPABASE_URL ?? '';
+const PUBLIC_SUPABASE_ANON_KEY = process.env.PUBLIC_SUPABASE_ANON_KEY ?? '';
+
+const supabaseUrl = PUBLIC_SUPABASE_URL ?? '';
+const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY ?? '';
+
+console.log(supabaseUrl);
+console.log(supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
 	throw new Error('SUPABASE_URL and SUPABASE_ANON_KEY must be set');
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,12 @@
+<form method="POST" action="?/login">
+	<label>
+		Email
+		<input name="email" type="email" class="input input-bordered" />
+	</label>
+	<label>
+		Password
+		<input name="password" type="password" class="input input-bordered" />
+	</label>
+	<button class="btn">Login</button>
+	<button formaction="?/signup" class="btn">Sign up</button>
+</form>
